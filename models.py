97c558c0af03812b9a16843099027025ca3af3d5
roheemot12 <o.yahya@alustudent.com>
2026-08-15 13:50:37 +0400

    Add __str__ method and test block to Assignment class

--- models.py
+++ models.py
@@ -12,3 +12,16 @@ class Assignment:
         if score > self.max_score:
             raise ValueError(f"Score cannot exceed max score of {self.max_score}.")
         self.score = score
+
+    def __str__(self):
+        if self.score is None:
+            return f"{self.title} ({self.subject}) - Not graded now"
+        percentage = (self.score / self.max_score) * 100
+        return f"{self.title} ({self.subject}) - {self.score}/{self.max_score} ({percentage:.1f}%)"
+
+
+
+if __name__ == "__main__":
+        test_assignment = Assignment("English HW 1 ", "English", "2026-09-15", 20)
+        test_assignment.record_score(15)
+        print(test_assignment) 
