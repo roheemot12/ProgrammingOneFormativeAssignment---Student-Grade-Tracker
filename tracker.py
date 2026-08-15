f2eb276eb342ccdb7eebb129631062d5bf613e36
roheemot12 <o.yahya@alustudent.com>
2026-08-15 17:48:36 +0400

    Add list_all, filter_by_subject, and filter_by_type methods

--- tracker.py
+++ tracker.py
@@ -4,3 +4,12 @@ class GradeTracker:
 
     def add_assignment(self, assignment):
             self.assignments.append(assignment)
+
+    def list_all(self):
+            return self.assignments
+
+    def filter_by_subject(self, subject):
+           return [a for a in self.assignments if a.subject.lower() == subject.lower()]
+    
+    def filter_by_type(self, type_name):
+         return [a for a in self.assignments if type(a).__name__.lower() ==  type_name.lower()] 
