f1a833e0db7f07085c73c30e104143c3d641151c
roheemot12 <o.yahya@alustudent.com>
2026-08-15 12:13:22 +0400

    Add Assignment class with constructor

---
+++ models.py
@@ -0,0 +1,7 @@
+class Assignment:
+    def __init__(self, title, subject, date, max_score, score=None):
+        self.title = title
+        self.subject = subject
+        self.date = date
+        self.max_score = max_score
+        self.score = score
