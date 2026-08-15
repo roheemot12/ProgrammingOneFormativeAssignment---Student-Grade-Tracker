3b46d7c51102e4845f61dd3b6bb160903f13c170
roheemot12 <o.yahya@alustudent.com>
2026-08-15 21:18:22 +0400

    Add add_homework function

--- main.py
+++ main.py
@@ -23,3 +23,31 @@ def get_positive_float(prompt):
 
         except ValueError:
             print(" ! Please enter a valid number. Try agin.")
+
+
+            #it will repeatedly asks for a score between 0 and max_score (inclusive)
+            def get_score_input(prompt, max_score) :
+                while True:
+                    raw_value = input(prompt).strip()
+                    try:
+                        value = float(raw_value)
+                        if value < 0 or value > max_score:
+                            print(f"  ! Please  dear enter a score between 0 and {max_score}.")
+                            continue
+                        return value
+                    except ValueError:
+                        print(" ! Please dear enter a valid number. Try again.")
+
+
+                        #this function below will ask the user for homework details and add it to the tracker.
+                        def add_homework(tracker):
+                            print("\n--- Add Homework ---")
+                            title = get_non_empty_input("Enter the title: ")
+                            subject = get_non_empty_input("Enter the subject: ")
+                            date = get_non_empty_input("Enter the date (YYYY-MM-DD): ")
+                            max_score = get_positive_float("Enter the maximum score: ")
+                            score = get_score_input(f"Enter score achieved (0-{max_score}): ", max_score)
+
+                            homework = Homework(title, subject, date, max_score, score)
+                            tracker.add_assignment(homework)
+                            print(f" Added : {homework}")
