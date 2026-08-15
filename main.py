2ff81ad2c8899e6b030096e70d1bcaf407ac4d00
roheemot12 <o.yahya@alustudent.com>
2026-08-15 20:21:09 +0400

    Add input helper functions for validation

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Student Grade/Assignment Tracker - setup OK")
+from models import Homework, Exam
+from tracker import GradeTracker
+
+# it will repeatedly asks the user to type something until they typer something (not blank).
+def get_non_empty_input(prompt):
+    while True:
+        value = input(prompt).strip()
+        if value:
+            return value
+        print(" ! This field cannot be emmpty. Try again.")
+
+
+        #It will repeatedly asks for a number grater than 0 (example: max score for an assignment)
+def get_positive_float(prompt):
+    while True:
+        raw_value = input(prompt).strip()
+        try:
+            value = float(raw_value)
+            if value <= 0:
+                print(" ! Please enter a number grater than 0.")
+                continue
+            return value
+
+        except ValueError:
+            print(" ! Please enter a valid number. Try agin.")
